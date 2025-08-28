cabb936179a8e33c95514445e11dc25e94941b95
Alexis Bourgeois <2277954@cegepmontpetit.ca>
2025-08-28 16:13:30 -0400

    fct fin

---
+++ exercice_drapeau_italien/android/app/src/main/kotlin/org/Bourgeois/exercice_drapeau_italien/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.Bourgeois.exercice_drapeau_italien
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ exercice_drapeau_italien/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body:Container (
+        color: Colors.black12,
+        //height: 500.0,
+        child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Row(
+        children: [
+        Expanded(
+
+          child: Container(
+
+            width: double.infinity,
+            color: Colors.green,
+            height: MediaQuery.of(context).size.height,
+            ),
+          ),
+
+        Expanded(
+          child: Container(
+
+            width: double.infinity,
+            color: Colors.white,
+            height: MediaQuery.of(context).size.height,
+          ),
+
+
+        ),
+
+          Expanded(
+            child: Container(
+
+              width: double.infinity,
+              color: Colors.red,
+              height: MediaQuery.of(context).size.height,
+            ),
+
+
+          )
+        ],
+      ),
+
+
+
+       ],
+        ),
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
